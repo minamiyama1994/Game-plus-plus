cbe7db6f623e62d2612cda14ccb6fc0656a7dd63
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-02-15 17:46:22 +0900

    success show image

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,13 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <FL/Fl_PNG_Image.H>
 
 auto main(int argc, char **argv) -> int
 {
-	Fl_Window * window = new Fl_Window ( 340 , 180 ) ;
-	Fl_Box * box = new Fl_Box ( 20 , 40 , 300 , 100 , "こんにちは世界" ) ;
-	box->box ( FL_UP_BOX ) ;
-	box->labelfont ( FL_BOLD + FL_ITALIC ) ;
-	box->labelsize ( 36 ) ;
-	box->labeltype ( FL_SHADOW_LABEL ) ;
+	Fl_Window * window = new Fl_Window { 820 , 620 } ;
+	Fl_Box * box = new Fl_Box { 10 , 10 , 800 , 600 /*, "こんにちは世界" */} ;
+	box->image ( new Fl_PNG_Image { "test.png" } ) ;
 	window->end ( ) ;
 	window->show ( argc , argv ) ;
 	return Fl::run ( ) ;
