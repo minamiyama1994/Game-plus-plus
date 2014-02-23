b18fb034afe52c3c3c504e60bbf1218fc450ce45
minamiyama1994 <masakazu.minamiyama@gmail.com>
2014-02-23 12:26:43 +0900

    success show window

--- test/test.cpp
+++ test/test.cpp
@@ -1,36 +1,24 @@
-#include<type_traits>
-#include"boost/thread/thread.hpp"
-#include"boost/chrono.hpp"
-#include<FL/Fl.H>
-#include<FL/Fl_Double_Window.H>
-#include<FL/Fl_Box.H>
-#include<FL/Fl_PNG_Image.H>
-namespace
-{
-	Fl_Double_Window * window = nullptr ;
-	Fl_Box * box = nullptr ;
-	Fl_PNG_Image * image = nullptr ;
+#include <fx.h>
+#include "test.hpp"
+
+FXDEFMAP(MainWindow) MainWindowMap[] = {};
+
+FXIMPLEMENT(MainWindow, FXMainWindow, MainWindowMap, ARRAYNUMBER(MainWindowMap))
+
+// 250x250のメインウインドウの大きさ
+MainWindow::MainWindow(FXApp *app)
+    : FXMainWindow(app, "MainWindow", NULL, NULL, DECOR_ALL, 0, 0, 250, 250) {}
+
+void MainWindow::create() {
+  FXMainWindow::create();
+  show(PLACEMENT_SCREEN); // PLACEMENT_SCREENで画面中央に表示
 }
-auto main(int argc, char **argv) -> int
-{
-	Fl::args ( argc , argv ) ;
-	window = new Fl_Double_Window { 820 , 620 } ;
-	box = new Fl_Box { 10 , 10 , 800 , 600 /*, "こんにちは世界" */ } ;
-	image = new Fl_PNG_Image { "test000.png" } ;
-	box->image ( image ) ;
-	window->add ( box ) ;
-	box->box ( FL_UP_BOX  ) ;
-	window->end ( ) ;
-	window->show ( ) ;
-	Fl::wait ( 5 ) ;
-	window->begin ( ) ;
-	window->remove ( box ) ;
-	//box = new Fl_Box { 10 , 10 , 800 , 600 /*, "こんにちは世界" */ } ;
-	image = new Fl_PNG_Image { "test001.png" } ;
-	box->image ( image ) ;
-	window->add ( box ) ;
-	box->box ( FL_UP_BOX  ) ;
-	window->end ( ) ;
-	window->show ( ) ;
-	return Fl::run ( ) ;
+
+MainWindow::~MainWindow() {}
+int main(int argc, char *argv[]) {
+  FXApp application("MainWindow", "MainWindow");
+  application.init(argc, argv);
+  new MainWindow(&application);
+  application.create();
+  return application.run();
 }
